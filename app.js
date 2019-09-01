58c08da8bc2a4a11419b8f2ee460e69b99462f0e
rvstowrn <44017653+rvstowrn@users.noreply.github.com>
2019-09-01 16:43:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,23 +10,23 @@ var el,i=0,x1,y1,h,w;
         h = parseInt(el.css("height").slice(0, -2));
         w = parseInt(el.css("width").slice(0, -2));
         if(x1>w &&  y1>h)
-        	el.attr("src","direction/se.jpg")
+        	el.attr("src","se.jpg")
         else if(x1>w && y1<0)
-        	el.attr("src","direction/ne.jpg")
+        	el.attr("src","ne.jpg")
         else if(x1<0 && y1>h)
-        	el.attr("src","direction/sw.jpg")
+        	el.attr("src","sw.jpg")
         else if(x1<0 && y1<0)
-        	el.attr("src","direction/nw.jpg")
+        	el.attr("src","nw.jpg")
         else if(x1<0)
-        	el.attr("src","direction/w.jpg")
+        	el.attr("src","w.jpg")
         else if(x1>w)
-        	el.attr("src","direction/e.jpg")
+        	el.attr("src","e.jpg")
         else if(y1>h)
-        	el.attr("src","direction/s.jpg")
+        	el.attr("src","s.jpg")
         else if(y1<0)	
-        	el.attr("src","direction/n.jpg")
+        	el.attr("src","n.jpg")
         else
-        	el.attr("src","direction/hover.jpg")
+        	el.attr("src","hover.jpg")
 
 
        // console.log(x1+" "+y1+" "+x2+" "+y2);
